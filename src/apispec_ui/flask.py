3ae7f7d41e2b315690277d42512bba0f8424f12e
Renato Teixeira <rena2damas@gmail.com>
2022-03-14 18:54:47 +0000

    Add swagger views

--- src/apispec_ui/flask.py
+++ src/apispec_ui/flask.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, MethodView
+from flask import Blueprint, jsonify, render_template, url_for
+from flask.views import MethodView
 
 
 class SwaggerUIView(MethodView):
@@ -6,49 +7,53 @@ class SwaggerUIView(MethodView):
     def __init__(self, *args, **kwargs):
         view_args = kwargs.pop('view_args', {})
         self.config = view_args.get('config')
-        self.apispec = view_args.get('apispec')
+        self.apispecs = view_args.get('apispecs')
         super(SwaggerUIView, self).__init__(*args, **kwargs)
 
     def get(self):
-        apispec = self.apispec
+        specs = self.apispecs.to_dict()
         data = {
-            "url": url_for("swagger.ui"),
-            "title": apispec["info"]["title"],
-            "version": apispec["info"]["version"],
-            "description": apispec["info"]["description"],
-            'favicon': self.config.get(
+            "url": url_for("swagger.specs"),
+            "title": specs["info"]["title"],
+            "version": specs["info"]["version"],
+            "description": specs["info"]["description"],
+            "favicon": self.config.get(
                 'favicon',
                 url_for('swagger.static', filename='favicon-32x32.png')
-            )
+            ),
+            **self.config
         }
-        return render_template(
-            'swagger-ui/index.html',
-            **data
-        )
+        return render_template("index.html", **data)
+
+
+class SwaggerSpecsView(MethodView):
+
+    def __init__(self, *args, **kwargs):
+        view_args = kwargs.pop('view_args', {})
+        self.apispecs = view_args['apispecs']
+        super(SwaggerSpecsView, self).__init__(*args, **kwargs)
+
+    def get(self):
+        return jsonify(self.apispecs.to_dict())
 
 
 class Swagger:
     DEFAULT_CONFIG = {
-        "headers": [],
-        "apispec": {
-            "endpoint": 'apispec',
-            "route": '/specs.json',
-            "rule_filter": lambda rule: True,  # all in
-            "model_filter": lambda tag: True,  # all in
-        },
         "swaggerui": True,
-        "swaggerui_static": "/swaggerui_static",
-        "swaggerui_route": "/swaggerui/"
+        "swagger_static": "/swagger_static/",
+        "swagger_route": "/swagger/"
     }
 
     def __init__(
             self,
-            apispec,
+            apispecs,
             app=None,
             config=None,
             merge=False
     ):
-        self.apispec = apispec
+        self.apispecs = apispecs
+        self.app = None
+
         self._init_config(config, merge)
         if app:
             self.init_app(app)
@@ -61,42 +66,45 @@ class Swagger:
             self.config = config
         elif not config:
             self.config = default_config
-        self.config.update(app.config.get('SWAGGER', {}))
 
     def init_app(self, app):
         self.app = app
 
-        # self.load_apispec(app)
+        # use SWAGGER as environment variable for configs
+        self.config.update(app.config.get('SWAGGER', {}))
+
         self.register_swagger(app)
 
-    def register_swaggerui(self, app):
+    def register_swagger(self, app):
         if self.config["swaggerui"]:
             blueprint = Blueprint(
                 "swagger",
                 __name__,
-                url_prefix=self.config.get('url_prefix', None)
+                url_prefix=self.config.get('url_prefix', ""),
+                template_folder="swagger-ui/",
+                static_folder="swagger-ui/",
+                static_url_path=self.config["swagger_static"]
             )
 
             blueprint.add_url_rule(
-                rule=self.config["swaggerui_route"],
+                rule=self.config["swagger_route"],
                 endpoint='ui',
                 view_func=SwaggerUIView().as_view(
                     name="swaggerui",
-                    view_args=dict(config=self.config, apispec=self.apispec)
+                    view_args=dict(config=self.config, apispecs=self.apispecs)
                 )
             )
 
         else:
             blueprint = Blueprint('swagger', __name__)
 
-        apispec = self.config.apispec
+        # add rule for swagger specs
         blueprint.add_url_rule(
-            rule=apispec['route'],
-            endpoint=apispec['endpoint'],
-            view_func=wrap_view(APISpecsView.as_view(
-                name=apispec['endpoint'],
-                view_args=self.apispec
-            ))
+            rule=f"{self.config['swagger_route']}/specs.json",
+            view_func=SwaggerSpecsView.as_view(
+                name="specs",
+                view_args=dict(apispecs=self.apispecs)
+            )
         )
 
         app.register_blueprint(blueprint)
