66c8298566408e50e4e9b54a08741722ead52b92
Renato Teixeira <rena2damas@gmail.com>
2022-03-15 12:41:25 +0000

    Linter

--- src/apispec_ui/flask.py
+++ src/apispec_ui/flask.py
@@ -3,12 +3,11 @@ from flask.views import MethodView
 
 
 class SwaggerUIView(MethodView):
-
     def __init__(self, *args, **kwargs):
-        view_args = kwargs.pop('view_args', {})
-        self.apispec = view_args.get('apispec')
-        self.config = view_args.get('config')
-        super(SwaggerUIView, self).__init__(*args, **kwargs)
+        view_args = kwargs.pop("view_args", {})
+        self.apispec = view_args.get("apispec")
+        self.config = view_args.get("config")
+        super().__init__(*args, **kwargs)
 
     def get(self):
         specs = self.apispec.to_dict()
@@ -18,20 +17,18 @@ class SwaggerUIView(MethodView):
             "version": specs["info"]["version"],
             "description": specs["info"].get("description"),
             "favicon": self.config.get(
-                'favicon',
-                url_for('swagger.static', filename='favicon-32x32.png')
+                "favicon", url_for("swagger.static", filename="favicon-32x32.png")
             ),
-            **self.config
+            **self.config,
         }
         return render_template("index.html", **data)
 
 
 class SwaggerSpecsView(MethodView):
-
     def __init__(self, *args, **kwargs):
-        view_args = kwargs.pop('view_args', {})
-        self.apispec = view_args['apispec']
-        super(SwaggerSpecsView, self).__init__(*args, **kwargs)
+        view_args = kwargs.pop("view_args", {})
+        self.apispec = view_args["apispec"]
+        super().__init__(*args, **kwargs)
 
     def get(self):
         return jsonify(self.apispec.to_dict())
@@ -41,15 +38,10 @@ class Swagger:
     DEFAULT_CONFIG = {
         "swaggerui": True,
         "swagger_static": "/swagger_static/",
-        "swagger_route": "/swagger/"
+        "swagger_route": "/swagger/",
     }
 
-    def __init__(
-            self,
-            apispec,
-            app=None,
-            config=None
-    ):
+    def __init__(self, apispec, app=None, config=None):
         self.apispec = apispec
         self.app = app
         default_config = self.DEFAULT_CONFIG.copy()
@@ -61,7 +53,7 @@ class Swagger:
         self.app = app
 
         # use SWAGGER as environment variable for configs
-        self.config.update(app.config.get('SWAGGER', {}))
+        self.config.update(app.config.get("SWAGGER", {}))
 
         self.register_swagger(app)
 
@@ -70,31 +62,30 @@ class Swagger:
             blueprint = Blueprint(
                 "swagger",
                 __name__,
-                url_prefix=self.config.get('url_prefix', ""),
+                url_prefix=self.config.get("url_prefix", ""),
                 template_folder="swagger-ui/",
                 static_folder="swagger-ui/",
-                static_url_path=self.config["swagger_static"]
+                static_url_path=self.config["swagger_static"],
             )
 
             blueprint.add_url_rule(
                 rule=self.config["swagger_route"],
-                endpoint='ui',
+                endpoint="ui",
                 view_func=SwaggerUIView().as_view(
                     name="swaggerui",
-                    view_args=dict(config=self.config, apispec=self.apispec)
-                )
+                    view_args=dict(config=self.config, apispec=self.apispec),
+                ),
             )
 
         else:
-            blueprint = Blueprint('swagger', __name__)
+            blueprint = Blueprint("swagger", __name__)
 
         # add rule for swagger specs
         blueprint.add_url_rule(
             rule=f"{self.config['swagger_route']}/specs.json",
             view_func=SwaggerSpecsView.as_view(
-                name="specs",
-                view_args=dict(apispec=self.apispec)
-            )
+                name="specs", view_args=dict(apispec=self.apispec)
+            ),
         )
 
         app.register_blueprint(blueprint)
