147b65d092332754329d6033fca8f8d254c6f41d
Renato Teixeira <rena2damas@gmail.com>
2022-03-15 12:36:17 +0000

    chore(ci): ignore ui dist files #wip

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -25,28 +25,22 @@ def spec(request):
 
 
 class TestFlask:
-
     def test_simple_app(self, app, spec):
-        Swagger(
-            app=app,
-            apispec=spec,
-            config={}
-        )
+        Swagger(app=app, apispec=spec, config={})
         client = app.test_client()
 
         assert url_for("swagger.ui") == "/swagger/"
         assert url_for("swagger.specs") == "/swagger/specs.json"
         assert client.get(url_for("swagger.specs")).json == spec.to_dict()
         assert "text/html" in client.get(url_for("swagger.ui")).headers["content-type"]
-        assert "application/json" in client.get(url_for("swagger.specs")).headers[
-            "content-type"]
+        assert (
+            "application/json"
+            in client.get(url_for("swagger.specs")).headers["content-type"]
+        )
 
     def test_lazy_init_app(self, spec):
         app = Flask(__name__)
-        swagger = Swagger(
-            apispec=spec,
-            config={}
-        )
+        swagger = Swagger(apispec=spec, config={})
         swagger.init_app(app)
 
         with app.test_request_context():
@@ -60,19 +54,15 @@ class TestFlask:
             config={
                 "swaggerui": True,
                 "swagger_static": "/test_static/",
-                "swagger_route": "/test/docs/"
-            }
+                "swagger_route": "/test/docs/",
+            },
         )
 
         assert url_for("swagger.ui") == "/test/docs/"
         assert url_for("swagger.specs") == "/test/docs/specs.json"
 
     def test_no_swagger_ui(self, app, spec):
-        Swagger(
-            app=app,
-            apispec=spec,
-            config={"swaggerui": False}
-        )
+        Swagger(app=app, apispec=spec, config={"swaggerui": False})
 
         assert url_for("swagger.specs") == "/swagger/specs.json"
         with pytest.raises(BuildError):
